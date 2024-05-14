849caf78a180ce1674aa9e12129439d7c49f5719
ShubhamYv <shubhamyadav32100@gmail.com>
2024-05-14 20:56:11 +0530

    Added entity classes and establish mappings. Introduce Chat, Comment, Invitation, Issue, Message, and Project entities with established mappings.

---
+++ src/main/java/com/sky/entity/Comment.java
@@ -0,0 +1,32 @@
+package com.sky.entity;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Comment {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	private String content;
+	private LocalDateTime createdDateTime;
+	
+	@ManyToOne
+	private User user;
+
+	@ManyToOne
+	private Issue issue;
+}
